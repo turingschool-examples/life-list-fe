47404cf13660fc5ddd4687636adfdfc6ec9132a0
Robbie Jaeger <jaeger.rob@gmail.com>
2023-05-23 21:44:14 -0600

    Remove boilerplate HTML and add content skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Life List</h1>
+
+      <form>
+        <input name="name" title="bird name" type="text" placeholder="Bird name..." />
+        <input name="date" title="date spotted" type="text" placeholder="Date spotted..." />
+        <input name="place" title="place spotted" type="text" placeholder="Place spotted..." />
+      </form>
+
+      <section>
+        <article>
+          <h3>Wren</h3>
+          <p>Date: 05-22-2023</p>
+          <p>Place: Under the sea.</p>
+        </article>
+      </section>
     </div>
   );
 }
